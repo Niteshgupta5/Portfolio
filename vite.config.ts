edb252de029d98fca10a784aa4b92c6bb050ca24
Nitesh Gupta <84310781+Niteshgupta5@users.noreply.github.com>
2026-09-08 15:05:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   define: {
     __BASE_PATH__: JSON.stringify('/'),
